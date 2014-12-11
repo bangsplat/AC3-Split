f6466ab6e634498020132304f135654a5d498f45
Bangsplat <theron@dankerton.net>
2014-12-10 17:03:38 -0800

    Initial commit
    splitAC3.pl - will split AC-3 stream where changes in format occur

---
+++ splitAC3.pl
@@ -0,0 +1,27 @@
+#!/usr/bin/perl
+use strict;	# Enforce some good programming rules
+
+#####
+#
+# splitAC3.pl
+#	version		0
+# 	created 	2014-12-10
+# 	modified	2014-12-10
+# 	author		Theron Trowbridge
+#
+# based on parseAC3.pl
+#
+# description
+# 
+# 
+# syntax
+# 
+# 
+# output
+#
+#
+#####
+
+
+
+
